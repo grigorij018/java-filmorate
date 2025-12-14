9b478719fb57752afed741f357629eb1ac4822c3
Kamila Suyargulova <kamila.suyargulova@yandex.ru>
2025-12-15 00:56:47 +0300

    Функциональность «Отзывы». 4 SP

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class Review {
+    private Integer reviewId;
+    private String content;
+    private Boolean isPositive;
+    private Integer userId;
+    private Integer filmId;
+    private Integer useful = 0;
+    private LocalDateTime createdAt;
+    private Set<Integer> likes = new HashSet<>();
+    private Set<Integer> dislikes = new HashSet<>();
+
+    public Integer calculateUseful() {
+        return (likes != null ? likes.size() : 0) - (dislikes != null ? dislikes.size() : 0);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.filmorate.storage.review;
+
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReviewStorage {
+    List<Review> findAll();
+
+    Review create(Review review);
+
+    Review update(Review review);
+
+    void delete(Integer id);
+
+    Optional<Review> findById(Integer id);
+
+    List<Review> findByFilmId(Integer filmId, Integer count);
+
+    List<Review> findAllWithLimit(Integer count);
+
+    void addLike(Integer reviewId, Integer userId);
+
+    void addDislike(Integer reviewId, Integer userId);
+
+    void removeLike(Integer reviewId, Integer userId);
+
+    void removeDislike(Integer reviewId, Integer userId);
+
+    void removeReaction(Integer reviewId, Integer userId);
+
+    boolean existsById(Integer id);
+}
